c769dc4995d766da5d85b55143385cbb7b3c37eb
EugeneMix <hhhiiiohtsu@gmail.com>
2025-03-03 03:26:36 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleDiscography() {
-    var discography = document.getElementById("discography");
-    if (discography.style.display === "none" || discography.style.display === "") {
-        discography.style.display = "block";
-    } else {
-        discography.style.display = "none";
-    }
-}
