498112e011e9d5e26e92652a25aac319cbf14957
EugeneMix <hhhiiiohtsu@gmail.com>
2025-03-02 15:27:07 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleDiscography() {
-    let content = document.getElementById("discography-content");
-    if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
-    } else {
-        content.style.display = "none";
-    }
-}
